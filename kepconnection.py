8e482ab100817d305a85eb07de1d1cf951f6e7dc
ministic2001 <kehxuan@gmail.com>
2023-05-11 11:27:31 +0800

    Updated some notes for the kepconnection.py and what modules to install

--- kepconnection.py
+++ kepconnection.py
@@ -1,5 +1,7 @@
 from kepconfig import connection
 
+# 172.16.2.77 if at lv 7
+# 172.16.2.223 if at lv 6
 server = connection.server(host = '172.16.2.77', port = 57412, user = 'Administrator', pw = 'administrator2022')
 
 print(server.get_info())
