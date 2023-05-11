2b05ccc8dd26c2509900f44961c5d839646bd299
Terence2389 <94297073+Terence2389@users.noreply.github.com>
2023-05-11 14:44:14 +0800

    update with user enable/disable

--- kepconnection.py
+++ kepconnection.py
@@ -1,7 +1,14 @@
-from kepconfig import connection
+from kepconfig import connection, admin
 
 # 172.16.2.77 if at lv 7
 # 172.16.2.223 if at lv 6
 server = connection.server(host = '172.16.2.77', port = 57412, user = 'Administrator', pw = 'administrator2022')
 
 print(server.get_info())
+
+print(admin.users.get_all_users(server))
+
+print(admin.users.enable_user(server, "User1"))
+
+print(admin.users.get_user(server, "User1" ))
+
