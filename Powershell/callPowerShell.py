5489957b32a1fb56d67601c53acae529a32349cd
NicholasChua <7358234+NicholasChua@users.noreply.github.com>
2023-05-18 17:00:44 +0800

    Add PS Command formatter + examples

--- Powershell/callPowerShell.py
+++ Powershell/callPowerShell.py
@@ -7,6 +7,7 @@ import os
 def getPowershellPath():
     """
     Helper function to get the path to the PowerShell executable. If it doesn't exist, stop the whole program.
+    
     Returns:
         string: The path to the PowerShell executable.
     """
@@ -44,15 +45,54 @@ def getPowershellPath():
     
     return ps_executable
 
-def callPowerShell(ip, command, outFile=None):
+
+def convertToCommand(command, filePath=None, parameters=None):
     """
-    Calls a PowerShell command and prints the output to the console.
-    If outFile is not None, then the output of the command will be written to the file specified by outFile.
+    Helper function to convert a command and optional filePaths and parameters into a properly formatted PowerShell command.
+    
     Inputs:
-        command: The PowerShell command to run.
-        outFile: The path to the file to write the output of the command to.
+        command: The command to run. Required.
+        filePath: The path to the file to run the command on. Optional.
+        parameters: The parameters to the command. Optional.
+    
+    Returns:
+        string: The command (and optional parameters) as a single string.
     """
 
+    # If filePath is None, then we don't have a file to run the command on
+    if filePath is None:
+        # If parameters is None, then we don't have any parameters to the command
+        if parameters is None:
+            # Return just the command
+            return command
+        # Else, we have parameters to the command
+        else:
+            # Return the command and parameters
+            return "{} -ArgumentList \'{}\'".format(command, parameters)
+    # Else, we have a file to run the command on
+    else:
+        # If parameters is None, then we don't have any parameters to the command
+        if parameters is None:
+            # Return the command and file path
+            return "{} -FilePath \'{}\'".format(command, filePath)
+        # Else, we have parameters to the command
+        else:
+            # Return the command, file path, and parameters
+            return "{} -FilePath \'{}\' -ArgumentList \'{}\'".format(command, filePath, parameters)
+
+
+def startPowershellSession(ip, command, outFile=None):
+    """
+    Starts a Powershell session, calls the specified command (with parameters if any), and prints the output to the console.
+    If outFile is specified, then the output of the command will be written to the file specified by outFile.
+    
+    Inputs:
+        ip: The IP address of the machine to run the command on. Required.
+        command: The PowerShell command to run. Call convertToCommand to get the Powershell command string. Required.
+        outFile: The path to the file to write the output of the command to. Optional.
+    """
+
+    # Call the helper function to get the path to the PowerShell executable
     ps_executable = getPowershellPath()
 
     # Import a CSV file with credentials
@@ -66,19 +106,63 @@ def callPowerShell(ip, command, outFile=None):
     username = credRow["Username"]
     password = credRow["Password"]
     # Create the PSSession (Broken on Windows with PS 5.1 on cmd on PS 7.3)
-    subprocess.call("{} New-PSSession -ComputerName {} -Credential (New-Object System.Management.Automation.PSCredential -ArgumentList {}, (ConvertTo-SecureString {} -AsPlainText -Force))".format(ps_executable, ip, username, password), shell=True)
+    subprocess.call('{} -Command \"New-PSSession -ComputerName {} -Credential (New-Object System.Management.Automation.PSCredential -ArgumentList {}, (ConvertTo-SecureString {} -AsPlainText -Force)\")'.format(ps_executable, ip, username, password), shell=True)
 
     # If outFile is not None, then we want to write the output of the command to a file
     if outFile:
         # Open the file for writing
         with open(outFile, "w") as f:
             # Run the command and write the output to the file
-            subprocess.call("{} {}".format(ps_executable, command), shell=True, stdout=f)
+            subprocess.call('{} -Command \"{}\"'.format(ps_executable, command), shell=True, stdout=f)
     # Else, just run and print the output of the command
     else:
-        subprocess.call("{} {}".format(ps_executable, command), shell=True)
+        subprocess.call('{} -Command \"{}\"'.format(ps_executable, command), shell=True)
 
     # Exit the PSSession
-    subprocess.call("{} Exit-PSSession".format(ps_executable), shell=True)
+    subprocess.call('{} -Command \"Exit-PSSession\"'.format(ps_executable), shell=True)
 
-callPowerShell("172.16.2.77", "Get-Service")
+
+def testPowershellLocally(command, outFile=None):
+    """
+    Copy of startPowershellSession, but for testing PowerShell commands locally.
+    Starts a Powershell session, calls the specified command (with parameters if any), and prints the output to the console.
+    If outFile is specified, then the output of the command will be written to the file specified by outFile.
+
+    Inputs:
+        command: The PowerShell command to run. Call convertToCommand to get the Powershell command string. Required.
+        outFile: The path to the file to write the output of the command to. Optional.
+    """
+    
+    # Call the helper function to get the path to the PowerShell executable
+    ps_executable = getPowershellPath()
+
+    # If outFile is not None, then we want to write the output of the command to a file
+    if outFile:
+        # Open the file for writing
+        with open(outFile, "w") as f:
+            # Run the command and write the output to the file
+            subprocess.call('{} -Command \"{}\"'.format(ps_executable, command), shell=True, stdout=f)
+    # Else, just run and print the output of the command
+    else:
+        print('{} -Command \"{}\"'.format(ps_executable, command))
+        subprocess.call('{} -Command \"{}\"'.format(ps_executable, command), shell=True)
+
+
+# Demo of how to use the functions, uncomment each block to try it out
+
+# Grab the services from the machine with IP 172.16.2.77
+# startPowershellSession(ip='172.16.2.77', cmd='Get-Service')
+
+# Grab the services from your machine and save it to Powershell\Get-Service.csv
+# testCmd = convertToCommand(command='Get-Service | Export-CSV -Path "Powershell\\Get-Service.csv"')
+# testPowershellLocally(command=testCmd)
+
+# Start Google Chrome in Dark Mode, Incognito, and navigate to https://www.google.com on your machine
+# testCmd = convertToCommand(command='Start-Process', filePath="C:\Program Files\Google\Chrome\Application\Chrome.exe", parameters='--force-dark-mode --incognito --new-window https://www.google.com')
+# testPowershellLocally(command=testCmd)
+
+# Disable the Windows Firewall on the machine with IP 172.16.2.77
+# startPowershellSession(ip='172.16.2.77', cmd='Set-NetFirewallProfile -All -Enabled False')
+
+# Enable the Windows Firewall on the machine with IP 172.16.2.77
+# startPowershellSession(ip='172.16.2.77', cmd='Set-NetFirewallProfile -All -Enabled True')
