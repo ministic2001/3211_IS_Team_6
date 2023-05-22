ad81c43011d8eaf5777644060faf0282017dc35c
eightyoneslides <44491830+Derrickeee@users.noreply.github.com>
2023-05-23 05:15:49 +0800

    Update ScriptToCaptureFileAndSendToDifferentDestination.sh

--- scripts/ScriptToCaptureFileAndSendToDifferentDestination.sh
+++ scripts/ScriptToCaptureFileAndSendToDifferentDestination.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sshpass -p 'Student12345@' ssh student@172.16.2.223 -o ConnectTimeout=5 "C:\Users\Student\Downloads\ScriptToCaptureFileAndSendToDifferentDestination.bat"
+sshpass -p 'Student12345@' ssh student@172.16.2.223 -o ConnectTimeout=5 "cd C:\Users\Student\Downloads\ScriptToCaptureFileAndSendToDifferentDestination.bat"
